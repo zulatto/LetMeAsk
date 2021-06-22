6162ad1cf53530a54feb283b5ea8dabc01608ec2
zulatto <zulatto.yve@gmail.com>
2021-06-22 16:55:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Home } from "./pages/Home";
+//import { Home } from "./pages/Home";
 
-//import { NewRoom } from "./pages/NewRoom";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <Home />
+    <NewRoom />
   );
 }
 
