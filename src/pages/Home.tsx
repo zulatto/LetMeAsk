7c9d55a0c2549ba1881952ab4a5d8e6ca537fee5
zulatto <zulatto.yve@gmail.com>
2021-06-21 23:01:41 -0300

    update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas"></img>
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
