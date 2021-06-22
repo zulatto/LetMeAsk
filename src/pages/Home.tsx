d499f6d95083fb8d17c97ffb09d402859392ed9d
zulatto <zulatto.yve@gmail.com>
2021-06-21 23:07:20 -0300

    update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+
 export function Home() {
     return (
         <div id="page-auth">
