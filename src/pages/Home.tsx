e55a8260c6af41ef53339c33b609be7676bcaf73
zulatto <zulatto.yve@gmail.com>
2021-06-21 23:24:14 -0300

    error correction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,33 +8,33 @@ export function Home() {
     return (
         <div id="page-auth">
             <aside>
-                <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas"></img>
+                <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
                 <p>
                     tire as duvidas de sua audiencia em tempo real
                 </p>
-                <main>
-                    <div>
-                        <img src={logoImg} alt="letmeask"></img>
-                        <button>
-                            <img src={googleIconImg} alt="Logo google"></img>
-                            Crie sua sala com o Google
-                        </button>
-                        <div>
-                            ou entre em nossa sala
-                        </div>
-                        <form>
-                            <input
-                                type="text"
-                                placeholder="Digite o codigo da sala"
-                            />
-                            <button type="submit">
-                                Entrar na sala
-                            </button>
-                        </form>
-                    </div>
-                </main>
             </aside>
+            <main>
+                <div>
+                    <img src={logoImg} alt="letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="Logo google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>
+                        ou entre em nossa sala
+                    </div>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Digite o codigo da sala"
+                        />
+                        <button type="submit">
+                            Entrar na sala
+                        </button>
+                    </form>
+                </div>
+            </main>
         </div>
     )
 }
