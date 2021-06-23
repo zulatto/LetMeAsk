4e0e4f98db98f081c1ad62f9c4a86a0247cae233
zulatto <zulatto.yve@gmail.com>
2021-06-23 18:39:40 -0300

    create room Q&A

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
+import logoImg from '../assets/images/Logo.svg'
+
+import { Button } from '../components/Button';
+
 export function Room(){
     return  (
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>Codigo</div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder="O que voce quer perguntar"
+                    />
+                    <div className="fomr-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                    <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     );
 }
