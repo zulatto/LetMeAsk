00d5079b16db90b7bad224d5a42f18adae70faa5
zulatto <zulatto.yve@gmail.com>
2021-06-23 18:04:08 -0300

    create flow enter in a room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,15 +8,35 @@ import { Button } from '../components/Button'
 import '../styles/auth.scss';
 
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent } from 'react';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() {
         const history = useHistory();
         const{user, signWithGoogle} = useAuth();
+        const [roomCode, setRoomCode] = useState('');
+
         async function handleCreateRoom(){
             if(!user){
                 await signWithGoogle()
             }
             history.push('/rooms/new');
+        }
+
+        async function handleJoinRoom(event: FormEvent) {
+            event.preventDefault();
+            if(roomCode.trim()==""){
+                return;
+            }
+            const roomRef = await database.ref(`romos/${roomCode}`).get();
+
+            if(!roomRef.exists()){
+                alert('Room does not exists');
+                return;
+            }
+            history.push(`/rooms/${roomCode}`);
+
         }
     return (
         <div id="page-auth">
@@ -37,10 +57,12 @@ export function Home() {
                     <div className="separator">
                         ou entre em nossa sala
                     </div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o codigo da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
