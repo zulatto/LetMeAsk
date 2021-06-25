1bb29d27436ee6b17ea2156b86ba6bdd267f09cc
zulatto <zulatto.yve@gmail.com>
2021-06-24 23:23:13 -0300

    ending function like

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { database } from '../services/firebase'
+import { database } from '../services/firebase';
+import {useAuth} from '../hooks/useAuth';
 
 type FirebaseQuestions= Record<string, {
     author: {
@@ -9,6 +10,9 @@ type FirebaseQuestions= Record<string, {
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likes: Record<string, {
+        authorId: string;
+    }>
 }>
 
 type QuestionType ={
@@ -20,11 +24,14 @@ type QuestionType ={
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likeCount: number;
+    likeId: string | undefined;
 
 }
 
 
 export function useRoom(roomId: string){
+    const { user } = useAuth();
     const [questions, setQuestions] = useState<QuestionType[]>([])
     const [title, setTitle] = useState('');
 
@@ -32,6 +39,7 @@ export function useRoom(roomId: string){
         const roomRef = database.ref(`rooms/${roomId}`);
 
         roomRef.on('value', room =>{
+
             const databaseRoom = room.val();
             const FirebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
@@ -42,12 +50,18 @@ export function useRoom(roomId: string){
                     author: value.author,
                     isHighlighted: value.isHighlighted,
                     isAnswered: value.isAnswered,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId == user?.id)?.[0],
                 }
             });
             setTitle(databaseRoom.title);
             setQuestions(parsedQuestions);
-        });
-    }, [roomId])
+        })
+
+        return() =>{
+            roomRef.off('value');
+        }
+    }, [roomId, user?.id]);
 
     return{questions, title}
 }
