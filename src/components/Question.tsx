51f595004ecc1861d8133e0f21a671a2bbde534b
zulatto <zulatto.yve@gmail.com>
2021-06-24 22:45:45 -0300

    create function like

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -5,12 +7,14 @@ type QuestionProps = {
     author: {
         name: string;
         avatar: string; 
-    }
+    };
+    children?: ReactNode;
 }
 
 export function Question({
     content,
     author,
+    children,
 }: QuestionProps){
     return(
         <div className="question">
@@ -22,7 +26,9 @@ export function Question({
                         {author.name}
                     </span>
                 </div>
-                <div></div>
+                <div>
+                    {children}
+                </div>
             </footer>
         </div>
     );
